48618aef20312a17afc2db0183495688e574a454
objliu <l1043261503@163.com>
2017-05-19 20:35:20 +0800

    commit

---
+++ app/src/main/java/liu/zhan/jun/androidpermissions/MainActivity.java
@@ -0,0 +1,99 @@
+package liu.zhan.jun.androidpermissions;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * 测试读写和打电话权限
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "LOGI";
+    private Button callPhone;
+    private Button readSd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        callPhone = (Button) findViewById(R.id.callphone);
+        readSd = (Button) findViewById(R.id.read_sd);
+        callPhone.setOnClickListener(this);
+        readSd.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.callphone:
+                callPhone();
+                break;
+            case R.id.read_sd:
+                break;
+        }
+    }
+
+    /**
+     * 拨打电话
+     */
+    private void callPhone() {
+        //6.0要判断是否有打电话的权限
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            //没有授权
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+            ActivityCompat.shouldShowRequestPermissionRationale(this, "=============");
+        } else {
+            //已授权
+            doCallPhone();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                //申请打电话权限回调grantResults[0] 对应请求的权限数组requestPermissions中的第二个数组参数
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doCallPhone();
+                } else {
+                    //权限未授予
+
+                    Log.i(TAG, "onRequestPermissionsResult:授权未通过");
+                }
+                break;
+        }
+    }
+
+    private void doCallPhone() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri data = Uri.parse("tel:10086");
+        intent.setData(data);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
+    }
+}
