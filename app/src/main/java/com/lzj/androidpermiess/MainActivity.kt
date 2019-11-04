a1738283634096e3fd136920a139c4434eb45ded
objliu <l1043261503@163.com>
2019-11-04 16:25:45 +0800

    commit

--- app/src/main/java/com/lzj/androidpermiess/MainActivity.kt
+++ app/src/main/java/com/lzj/androidpermiess/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.lzj.androidpermiess
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.lzj.androidpermiess.utils.PermissionUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //还必须在Manifest清单中声明
+        PermissionUtils.needPermission(
+            this, arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE
+            )
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionUtils.onRequestPermissionsResult(requestCode, permissions, grantResults,
+            success = {
+                Toast.makeText(this, "请求成功", Toast.LENGTH_SHORT).show()
+            },
+            fail = {
+                Toast.makeText(this, "${it}请求失败", Toast.LENGTH_SHORT).show()
+            })
     }
 }
